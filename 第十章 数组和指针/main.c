0ebbc9b78ad02a9ff71a23f4c3ef24b7ca806fe1
Affee <304442951@qq.com>
2018-05-11 15:01:23 +0800

    Initial Commit

---
+++ 第十章 数组和指针/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  第十章 数组和指针
+//
+//  Created by Affee on 2018/5/11.
+//  Copyright © 2018年 affee. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
