0857bd898859b59699d3dc92c819efc91a8cbe9c
Affee <304442951@qq.com>
2018-05-23 20:46:22 +0800

    尝试提交10.777

---
+++ 10.7 指针和多维数组/main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  10.7 指针和多维数组
+//
+//  Created by Affee on 2018/5/23.
+//  Copyright © 2018年 affee. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    printf("我是多维数组的啊，嘤嘤嘤，多维数组");
+    return 0;
+}
